41535e19e428b9e6f4d1ea252ffe6517c2474acb
Alex <alexandrotaboada@gamil.com>
2021-10-11 22:42:58 -0300

    Team more efficient

--- src/app/pages/team/team.component.ts
+++ src/app/pages/team/team.component.ts
@@ -15,9 +15,9 @@ export class TeamComponent implements OnInit {
   team:TeamModel
   teamPlayers:PlayerModel[]
   constructor(private service:MainService, private route: ActivatedRoute) {
+    var id = route.snapshot.params.id
     this.service.getSection('teams').subscribe((team:any)=>{
-      this.team = team.filter((team:TeamModel) => team.id == route.snapshot.params.id)
-      this.team = this.team[0]
+      this.team = team.filter((team:TeamModel) => team.id == id)[0]
       this.service.getSection('players').subscribe((players:any)=>{
         this.loading = false
         this.teamPlayers = players.filter((player:PlayerModel)=>player.teamId == this.team.id)
